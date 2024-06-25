81381173480980ff02a1a7ec75fb1eb967a2afaf
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-06-26 00:44:57 +0100

    ic

--- examples/jupiter-swap/route.ts
+++ examples/jupiter-swap/route.ts
@@ -111,10 +111,8 @@ app.openapi(
     }
     const lastTx = await connection.getTransaction(sigs[sigs.length-1].signature)
     gameState.leader = lastTx?.transaction.message.accountKeys[0] as PublicKey
-
-
     const { account } = (await c.req.json()) as { account: string; solAmount: number };
-const solAmount = ((gameState.lastSol / 10 ** 9) + 1) * 10 ** 9;
+const solAmount = ((gameState.lastSol ) + 1);
     if (new BN(solAmount).toNumber() <= gameState.lastSol) {
       return c.json({
         message: `You need to send at least ${gameState.lastSol / 10 ** 9} SOL to play.`,
