2c9b727ef4ea791012261c33763e86eab24aa0e0
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-06-25 16:51:13 +0100

    ic

--- examples/index.ts
+++ examples/index.ts
@@ -44,7 +44,7 @@ app.get(
   }),
 );
 
-const port = 3000;
+const port =  Number(process.env.PORT) || 3000;
 console.log(
   `Server is running on port ${port}
 Visit http://localhost:${port}/swagger-ui to explore existing actions
